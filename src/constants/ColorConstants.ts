f253ff877d6e3a5013919d20d2307a6666558d44
Sarbajit Biswal <sarbajeet.biswal@gmail.com>
2023-12-01 23:24:17 +0530

    fixed matine color issue

---
+++ src/constants/ColorConstants.ts
@@ -0,0 +1,5 @@
+export const ColorDao = {
+    primaryColor: "#d67dc4",
+    textColor: "#010101",
+    mutedColor: "#686868",
+}
--- src/main.tsx
+++ src/main.tsx
@@ -5,13 +5,15 @@ import "./index.css";
 import { RootRoute, Router, RouterProvider } from "@tanstack/react-router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { MantineProvider, createTheme } from '@mantine/core';
-
+import '@mantine/core/styles.css';
 // Create a root route
 const rootRoute = new RootRoute({
   component: App,
 });
 const theme = createTheme({
   /** Put your mantine theme override here */
+  primaryColor: 'pink',
+  primaryShade:6
 });
 // Create the route tree using your routes
 const routeTree = rootRoute.addChildren([]);
@@ -21,11 +23,13 @@ const router = new Router({ routeTree });
 // Create a client
 const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")!).render(
+  <MantineProvider theme={theme} defaultColorScheme="light">
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-    <MantineProvider theme={theme}>
+    
       <RouterProvider router={router} />
-      </MantineProvider>
+      
     </QueryClientProvider>
   </React.StrictMode>
+  </MantineProvider>
 );

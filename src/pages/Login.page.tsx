05ff14dff9cfbf013bac0ed11ed709fc1bbd0527
Sarbajit Biswal <sarbajeet.biswal@gmail.com>
2023-12-04 22:07:22 +0530

    skelton of login page

--- src/pages/Login.page.tsx
+++ src/pages/Login.page.tsx
@@ -1,22 +1,45 @@
-import { Text, Paper, Button, Grid, GridCol, Image, Box, Container, Center } from '@mantine/core';
+import { Text, Paper, Button, Grid, GridCol, Image, Box, Container, Title, Flex, Stack, TextInput, PasswordInput } from '@mantine/core';
 import splash from '../assets/splash.png';
+import classes from './Login.module.css';
+import { useForm } from '@mantine/form';
 
 export function LoginPage() {
+  const form = useForm({
+    initialValues: {
+      email: '',
+      termsOfService: false,
+    },
+
+    validate: {
+      email: (value) => (/^\S+@\S+$/.test(value) ? null : 'Invalid email'),
+    },
+  });
   return (
-    <Container fluid h="100%">
-      <Center>
-        <Paper shadow="md" withBorder p="xl">
+    <Container fluid style={{ height: '100%' }}>
+        <Paper shadow="md" withBorder p="xl" className={classes.paper}>
           <Grid>
             <GridCol span={6}>
-              <Box h={100} w="auto" bg="cyan" />
-              <Box h={100} w="auto" bg="red" />
+             <Box className={classes.form}>
+              <Stack h="100%" align="flex-start">
+                <Stack h="250" justify="flex-end">
+                <Title>Welcome back to Saving group</Title>
+                <Text>It's great to have you back</Text>
+                </Stack>
+                <Stack>
+                <form onSubmit={form.onSubmit((values) => console.log(values))}>
+                  <TextInput label="Username"/>
+                  <PasswordInput label="Password" />
+                </form>
+                </Stack>
+              </Stack>
+              <div className={classes.bottomBox} />
+             </Box>
             </GridCol>
             <GridCol span={6}>
-              <Image src={splash} radius="md" h={200} w="auto" fit="contain" />
+              <Image src={splash} className={classes.image} fit="initial" />
             </GridCol>
           </Grid>
         </Paper>
-      </Center>
     </Container>
   );
 }

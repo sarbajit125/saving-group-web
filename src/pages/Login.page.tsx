91b18edd464abb82e6c98ca4156a643d92f1b439
Sarbajit Biswal <sarbajeet.biswal@gmail.com>
2023-12-04 23:08:47 +0530

    added form for login ui

--- src/pages/Login.page.tsx
+++ src/pages/Login.page.tsx
@@ -1,7 +1,24 @@
-import { Text, Paper, Button, Grid, GridCol, Image, Box, Container, Title, Flex, Stack, TextInput, PasswordInput } from '@mantine/core';
+import {
+  Text,
+  Paper,
+  Button,
+  Grid,
+  GridCol,
+  Image,
+  Box,
+  Container,
+  Title,
+  Stack,
+  TextInput,
+  PasswordInput,
+  Group,
+  Center,
+  Anchor,
+  Checkbox,
+} from '@mantine/core';
+import { useForm } from '@mantine/form';
 import splash from '../assets/splash.png';
 import classes from './Login.module.css';
-import { useForm } from '@mantine/form';
 
 export function LoginPage() {
   const form = useForm({
@@ -16,30 +33,48 @@ export function LoginPage() {
   });
   return (
     <Container fluid style={{ height: '100%' }}>
-        <Paper shadow="md" withBorder p="xl" className={classes.paper}>
-          <Grid>
-            <GridCol span={6}>
-             <Box className={classes.form}>
-              <Stack h="100%" align="flex-start">
+      <Paper shadow="md" withBorder p="xl" className={classes.paper}>
+        <Grid>
+          <GridCol span={6}>
+            <Box className={classes.form}>
+              <Stack h="100%" align="streach" mr="md">
                 <Stack h="250" justify="flex-end">
-                <Title>Welcome back to Saving group</Title>
-                <Text>It's great to have you back</Text>
+                  <Title>Welcome back to Saving group</Title>
+                  <Text>It's great to have you back</Text>
                 </Stack>
-                <Stack>
-                <form onSubmit={form.onSubmit((values) => console.log(values))}>
-                  <TextInput label="Username"/>
-                  <PasswordInput label="Password" />
-                </form>
+                <Stack mt="md">
+                  <form onSubmit={form.onSubmit((values) => console.log(values))}>
+                    <TextInput label="Username" mb="sm" />
+                    <PasswordInput label="Password" mb="sm" />
+                    <Group justify="space-between">
+                      <Checkbox label="Remember me?" variant="outline" />
+                      <Anchor underline="never" c="black">
+                        Forgot password?
+                      </Anchor>
+                    </Group>
+                  </form>
                 </Stack>
+                <Group grow>
+                  <Button variant="filled"> Login </Button>
+                  <Button variant="outline"> Create account </Button>
+                </Group>
+                <Center mt="md">
+                  <Stack align="center" gap="xs">
+                    <Text>Or login with </Text>
+                    <Group>
+                      <Anchor underline="never">Facebook</Anchor>
+                      <Anchor underline="never">Google</Anchor>
+                    </Group>
+                  </Stack>
+                </Center>
               </Stack>
-              <div className={classes.bottomBox} />
-             </Box>
-            </GridCol>
-            <GridCol span={6}>
-              <Image src={splash} className={classes.image} fit="initial" />
-            </GridCol>
-          </Grid>
-        </Paper>
+            </Box>
+          </GridCol>
+          <GridCol span={6}>
+            <Image src={splash} className={classes.image} fit="initial" />
+          </GridCol>
+        </Grid>
+      </Paper>
     </Container>
   );
 }

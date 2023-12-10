0c7d74dbf5da0ee4b624f9a4a81335cc7f81ca05
Sarbajit Biswal <sarbajit.biswal@comviva.com>
2023-12-10 22:24:29 +0530

    added error boundary component

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,40 @@
 import '@mantine/core/styles.css';
+import 'react-toastify/dist/ReactToastify.css';
 import './app.css';
 import { MantineProvider } from '@mantine/core';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { QueryCache, QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ToastContainer, toast } from 'react-toastify';
 import { AppRouter } from './Router';
 import { theme } from './theme';
-
+import { RootErrorResponse } from './models/responseModels';
+import ErrorBoundary from './boundary/ErrorBoundary';
 // Create a client
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  queryCache: new QueryCache({
+    onError(error, query) {
+      if (query.meta && query.meta.errorMessage && query.meta.errorMessage instanceof String) {
+        toast.error(query.meta.errorMessage, { position: 'bottom-center', autoClose: 1000 });
+      } else if (error instanceof RootErrorResponse) {
+          if (error.statusCode >= 400 && error.statusCode < 500) {
+            toast.error(error.userMsg, { position: 'bottom-center', autoClose: 1000 });
+          } else {
+            // Go to something went wrong page
+          }
+        } else {
+          // Go to something went wrong page
+        }
+    },
+  }),
+});
 export default function App() {
   return (
+    <ErrorBoundary>
     <MantineProvider theme={theme}>
       <QueryClientProvider client={queryClient}>
+        <ToastContainer />
         <AppRouter />
       </QueryClientProvider>
     </MantineProvider>
+    </ErrorBoundary>
   );
 }
---
+++ src/boundary/ErrorBoundary.tsx
@@ -0,0 +1,44 @@
+import React, { Component, ErrorInfo } from 'react';
+import errorImg from '../assets/genericError.jpg';
+
+interface Props {
+  children: React.ReactNode;
+}
+
+interface State {
+  hasError: boolean;
+}
+
+class ErrorBoundary extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+    console.error('ErrorBoundary caught an error: ', error, errorInfo);
+    this.setState({ hasError: true });
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return (
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            textAlign: 'center',
+            minHeight: '100vh',
+          }}
+        >
+          <img src={errorImg} alt="Error boundary" width="100%" height="100%" />
+        </div>
+      );
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;

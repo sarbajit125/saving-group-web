7f1da8ce27fd95d9019cc7ac2d76726d3418dca0
Sarbajit Biswal <sarbajeet.biswal@gmail.com>
2023-12-28 20:37:36 +0530

    added join group modal

--- src/pages/GroupList.page.tsx
+++ src/pages/GroupList.page.tsx
@@ -10,7 +10,10 @@ import {
   Stack,
   Avatar,
   Button,
+  Modal,
+  TextInput,
 } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
 import { IoListOutline, IoGridOutline, IoAdd } from 'react-icons/io5';
 import { PiUsersLight } from 'react-icons/pi';
 import SideNavBar from '../components/SideNavBar/SideNavBar';
@@ -79,6 +82,7 @@ function GroupList() {
       invitedBy: 'Harshal Sehgal',
     },
   ];
+  const [opened, { open, close }] = useDisclosure(false);
   return (
     <Grid>
       <GridCol span={2}>
@@ -107,11 +111,14 @@ function GroupList() {
             <Card shadow="sm" padding="lg" radius="md" h={200} withBorder key={item.groupCode}>
               <Center h={200}>
                 <Stack>
-                    <Avatar src={item.groupImage} size="lg" style={{ alignSelf: 'center' }}>
-                      <PiUsersLight fontSize="4em" />
-                    </Avatar>
+                  <Avatar src={item.groupImage} size="lg" style={{ alignSelf: 'center' }}>
+                    <PiUsersLight fontSize="4em" />
+                  </Avatar>
                   <Box>
-                    <Text fw="bold" style={{ textAlign: 'center' }}> {item.groupName} </Text>
+                    <Text fw="bold" style={{ textAlign: 'center' }}>
+                      {' '}
+                      {item.groupName}{' '}
+                    </Text>
                     <Text style={{ textAlign: 'center' }}> Members: {item.memberCount}</Text>
                   </Box>
                 </Stack>
@@ -123,7 +130,27 @@ function GroupList() {
       <GridCol span={4}>
         <Group p="md" mt="xl" mr="sm" ml="sm" justify="space-between">
           <Text>Pending Invite</Text>
-          <Button color={ColorDao.primaryColor}>Join Group</Button>
+          <Button onClick={open} color={ColorDao.primaryColor}>
+            Join Group
+          </Button>
+          <Modal opened={opened} onClose={close} title="Join Group">
+            <TextInput placeholder="Enter group code" />
+            <Stack mt="md">
+              <Center h={100}>
+                <Stack>
+                  <Avatar src={null} size="lg" style={{ alignSelf: 'center' }}>
+                    <PiUsersLight fontSize="4em" />
+                  </Avatar>
+                  <Box>
+                    <Text fw="bold" style={{ textAlign: 'center' }}>
+                      Alok
+                    </Text>
+                  </Box>
+                </Stack>
+              </Center>
+              <Button color={ColorDao.primaryColor}>Send Request</Button>
+            </Stack>
+          </Modal>
         </Group>
         <Stack mr="sm" ml="sm">
           {dummyInviteList.map((item) => (
@@ -133,14 +160,20 @@ function GroupList() {
                   <PiUsersLight fontSize="2em" />
                 </Avatar>
                 <Box>
-                <Text size="sm" fw="bold">{item.groupName}</Text>
+                  <Text size="sm" fw="bold">
+                    {item.groupName}
+                  </Text>
                   <Text size="xs">Invited By: {item.invitedBy}</Text>
                   <Text size="xs">5 Friends 41,000 members</Text>
                 </Box>
               </Group>
               <Group>
-                <Button variant="outline" color={ColorDao.primaryColor}>Join</Button>
-                <Button variant="outline" color={ColorDao.goldColor}>Decline</Button>
+                <Button variant="outline" color={ColorDao.primaryColor}>
+                  Join
+                </Button>
+                <Button variant="outline" color={ColorDao.goldColor}>
+                  Decline
+                </Button>
               </Group>
             </Group>
           ))}
